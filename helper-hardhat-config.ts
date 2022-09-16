53c9e6b7c791a59bbb2549f01cfaee868914cea0
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-09-15 20:59:47 -0400

    goerli

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -21,14 +21,14 @@ export const networkConfig: networkConfigInfo = {
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
     },
-    4: {
-        name: "rinkeby",
+    5: {
+        name: "goerli",
         subscriptionId: "588",
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
         callbackGasLimit: "500000", // 500,000 gas
-        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab"
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D"
     },
     1: {
         name: "mainnet",
